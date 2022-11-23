3fffef03f8efcf6a5460cca0641a072df81c8655
ygritte723 <3190105744@zju.edu.cn>
2022-11-23 16:38:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Relatioanal Embedding for Few-Shot Clasification (ICCV 2021)
+will be released soon.
---
+++ models/dataloader/.ipynb_checkpoints/samplers-checkpoint.py
@@ -0,0 +1,43 @@
+import torch
+import numpy as np
+
+
+class CategoriesSampler():
+
+    def __init__(self, label, n_batch, n_cls, n_per):
+        # print(label, n_batch, n_cls, n_per)
+        self.n_batch = n_batch  # the number of iterations in the dataloader
+        self.n_cls = n_cls #2
+        self.n_per = n_per #20
+
+        label = np.array(label)  # all data label
+        #print(label)
+        self.m_ind = []  # the data index of each class
+        for i in range(max(label) + 1):
+            ind = np.argwhere(label == i).reshape(-1)  # all data index of this class
+            ind = torch.from_numpy(ind)
+            #print(ind)
+            self.m_ind.append(ind)
+        # print(self.m_ind)
+
+    def __len__(self):
+        return self.n_batch
+    
+    def __iter__(self):
+        for i_batch in range(self.n_batch):
+            batch = []
+            # print(len(self.m_ind))
+            classes = torch.randperm(len(self.m_ind))[:self.n_cls]  # random sample num_class indices, e.g. 5
+            # print(classes)
+            for c in classes:
+                l = self.m_ind[c]  # all data indices of this class
+            
+                pos = torch.randperm(len(l))[:self.n_per]  # sample n_per data index of this class
+                batch.append(l[pos])
+                # print(len(l[pos]))
+            batch = torch.stack(batch).t().reshape(-1)
+            # .t() transpose,
+            # due to it, the label is in the sequence of abcdabcdabcd form after reshape,
+            # instead of aaaabbbbccccdddd
+            yield batch
+
